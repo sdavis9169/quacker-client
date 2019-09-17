6cab78d5a121890b1251591620be04240ad8ff3e
Sdavis <sdavis@Stevens-MacBook-Pro.local>
2019-09-17 12:55:47 -0600

    data reducer

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid'
-import axios from 'axios'
 import Scream from '../components/Scream'
 import Profile from '../components/Profile'
+import PropTypes from 'prop-types'
+
+import { connect } from 'react-redux'
+import { getScreams} from '../redux/actions/dataActions'
 
 class home extends Component {
-    state = {
-        screams: null
-    }
     componentDidMount(){
-        axios.get('/screams')
-            .then(res => {
-                this.setState({
-                    screams: res.data
-                })
-            })
-            .catch(err => console.log(err))
+        this.props.getScreams()
     }
     render() {
-        let recentScreamsMarkup = this.state.screams ? (
-            this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
+      const { screams, loading } = this.props.data;
+        let recentScreamsMarkup = !loading ? (
+            screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
         ) :(
             <p>Loading...</p>
         ) 
@@ -36,4 +31,12 @@ class home extends Component {
     }
 }
 
-export default home
+home.propTypes = {
+  getScreams: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  data: state.data
+})
+
+export default connect(mapStateToProps, { getScreams })(home)
--- src/pages/login.js
+++ src/pages/login.js
@@ -3,7 +3,6 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import icon from '../images/icon.png'
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 // MUI Stuff
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
